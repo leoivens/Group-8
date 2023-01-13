9b547caf3f90a7ab3ab9973a94712a41dfbc13ca
leoivens <122292096+leoivens@users.noreply.github.com>
2023-01-13 17:32:38 +0100

    Update scan_echo.py

--- tuto_move/tuto_move/scan_echo.py
+++ tuto_move/tuto_move/scan_echo.py
@@ -14,31 +14,18 @@ class ScanInterpret(Node):
         self.scan_publisher = self.create_publisher(PointCloud, '/scan_node', 10)
 
     def scan_callback(self, scanMsg):
-        obstacles= []
+        pointcloud = PointCloud()
         angle= scanMsg.angle_min
         for aDistance in scanMsg.ranges :
-        	if 0.1 < aDistance and aDistance < 5.0 :
-        		aPoint= [
-         		    math.cos(angle) * aDistance,
-           		    math.sin(angle) * aDistance
-          	        ]
-        		obstacles.append(aPoint)
-        	angle+= scanMsg.angle_increment
-        sample= [ [ round(p[0], 2), round(p[1], 2) ] for p in  obstacles[10:20] ]
-        self.get_logger().info( f" obs({len(obstacles)}) ...{sample}..." )
+            if 0.1 < aDistance and aDistance < 5.0 :
+                aPoint= Point32()
+                aPoint.x= (float)(math.cos(angle) * aDistance)
+                aPoint.y= (float)(math.sin(angle) * aDistance)
+                aPoint.z= (float)(0)
+                pointcloud.points.append(aPoint)
+            angle+= scanMsg.angle_increment
         
-        aPoint= Point32()
-        aPoint.x= (float)(math.cos(angle) * aDistance)
-        aPoint.y= (float)(math.sin(angle) * aDistance)
-        aPoint.z= (float)(0)
-
-        self.points.append(aPoint)
-
-        pointcloud = PointCloud()
-        pointcloud.points = self.points
         pointcloud.header.frame_id = "laser"
-
-
         self.scan_publisher.publish(pointcloud)
 
 def main(args=None):
